1e530e29ee7023824135fc1afec52f61974d3df9
Dominik0914 <rozsa.dominik.2024d@telekimezotur.hu>
2026-09-09 13:13:32 +0200

    Add project files.

---
+++ gyak2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Add meg a termék nevét:");
+string termek = Console.ReadLine();
+Console.WriteLine("Add meg a termék darabszámát:");
+int termekdb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Add meg a termék nevét:");
+string termek2 = Console.ReadLine();
+Console.WriteLine("Add meg a termék darabszámát:");
+int termekdb2 = Convert.ToInt32(Console.ReadLine());
